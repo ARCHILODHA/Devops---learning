42248bcee93fdfacac098ccb2cc9b74aaa8b18d6
Archi Lodha <archilodha1@gmail.com>
2026-06-15 06:08:37 +0530

    Add docker_cleanup.sh for cleaning unused Docker resources
    This script helps to free disk space by removing unused Docker resources including dangling images, stopped containers, and unused volumes.

---
+++ Mini-Projects/docker_cleanup.sh
@@ -0,0 +1,11 @@
+# Docker Cleanup
+
+Frees disk space by removing unused Docker resources.
+
+## Features
+- Remove dangling images
+- Remove stopped containers
+- Remove unused volumes
+
+## Usage
+./docker_cleanup.sh
